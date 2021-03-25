16131e145e693b2964ba7601669d90a71362eb11
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2021-03-25 20:21:35 -0300

    challenge completed

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -6,11 +6,18 @@ import {
 } from 'react';
 
 import { useField } from '@unform/core';
+import { IconBaseProps } from 'react-icons/lib';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string;
+  placeholder?: string;
+  icon?: React.ComponentType<IconBaseProps>
+}
+
+export function Input({ name, icon: Icon, ...rest }: InputProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -23,7 +30,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
 
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
-
+  
     setIsFilled(!!inputRef.current?.value);
   }, []);
 
@@ -36,18 +43,16 @@ const Input = ({ name, icon: Icon, ...rest }) => {
   }, [fieldName, registerField]);
 
   return (
-    <Container isFilled={isFilled} isFocused={isFocused}>
+    <Container isFilled={ isFilled } isFocused={ isFocused }>
       {Icon && <Icon size={20} />}
-
+  
       <input
-        onFocus={handleInputFocus}
-        onBlur={handleInputBlur}
-        defaultValue={defaultValue}
-        ref={inputRef}
-        {...rest}
+        onFocus={ handleInputFocus }
+        onBlur={ handleInputBlur }
+        defaultValue={ defaultValue }
+        ref={ inputRef }
+        { ...rest }
       />
     </Container>
   );
-};
-
-export default Input;
+}
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,122 @@
+import { useEffect, useState } from 'react';
+
+import api from '../../services/api';
+
+import { Header } from '../../components/Header';
+import { Food } from '../../components/Food';
+import { ModalAddFood } from '../../components/ModalAddFood';
+import { ModalEditFood } from '../../components/ModalEditFood';
+
+import { FoodsContainer } from './styles';
+
+interface OneFood {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  available: boolean;
+  image: string;
+};
+
+interface DataProps {
+  image: string;
+  name: string;
+  price: string;
+  description: string;
+};
+
+export function Dashboard() {
+  const [foods, setFoods] = useState<OneFood[]>([]);
+  const [editingFood, setEditingFood] = useState<OneFood>({} as OneFood);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [editModalOpen, setEditModalOpen] = useState(false);
+
+  useEffect(() => {
+    async function getFoods() {
+      const response = await api.get('/foods');
+      setFoods(response.data);
+    };
+
+    getFoods();
+  }, []);
+
+  const handleAddFood = async (food: DataProps): Promise<void> => {
+    try {
+      const response = await api.post('/foods', {
+        ...food,
+        available: true,
+      });
+
+      setFoods([...foods, response.data]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleUpdateFood = async (food: DataProps): Promise<void> => {
+    try {
+      const foodUpdated = await api.put(
+        `/foods/${ editingFood.id }`,
+        { ...editingFood, ...food },
+      );
+
+      const foodsUpdated = foods.map(f =>
+        f.id !== foodUpdated.data.id ? f : foodUpdated.data,
+      );
+
+      setFoods(foodsUpdated);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const handleDeleteFood = async (id: number) => {
+    await api.delete(`/foods/${ id }`);
+
+    const foodsFiltered = foods.filter(food => food.id !== id);
+
+    setFoods(foodsFiltered);
+  }
+
+  const toggleModal = () => {
+    setModalOpen(!modalOpen);
+  }
+
+  const toggleEditModal = () => {
+    setEditModalOpen(!editModalOpen);
+  }
+
+  const handleEditFood = (food: OneFood) => {
+    setEditModalOpen(true);
+    setEditingFood(food);
+  }
+
+  return (
+    <>
+      <Header openModal={ toggleModal } />
+      <ModalAddFood
+        isOpen={ modalOpen }
+        setIsOpen={ toggleModal }
+        handleAddFood={ handleAddFood }
+      />
+      <ModalEditFood
+        isOpen={ editModalOpen }
+        setIsOpen={ toggleEditModal }
+        editingFood={ editingFood }
+        handleUpdateFood={ handleUpdateFood }
+      />
+
+      <FoodsContainer data-testid="foods-list">
+        {foods &&
+          foods.map(food => (
+            <Food
+              key={ food.id }
+              food={ food }
+              handleDelete={ handleDeleteFood }
+              handleEditFood={ handleEditFood }
+            />
+          ))}
+      </FoodsContainer>
+    </>
+  );
+}
